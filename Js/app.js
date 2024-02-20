9f845189a1f55da9b0782c4e7e5dd5351450cd2f
whoamihack1ng <whoamihack1ng@gmail.com>
2024-02-21 02:03:00 +0500

    loyiha tugatildi!

--- Js/app.js
+++ Js/app.js
@@ -5,3 +5,9 @@ const Menu = document.querySelector('#menu')
 NavbarMenu.addEventListener('click',()=>{
     Menu.classList.toggle("block")
 })
+
+// Bu yerda yilni chiqaruvchi paragraph chaqirildi!
+const year = document.querySelector('#year')
+
+const data = new Date().getFullYear()
+year.innerHTML = data;
